16274116b91b1d38e2efd306e51ebcdfc4253970
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2019-05-14 22:26:20 +0200

    [TASK] Add PackageStates.php

---
+++ public/typo3conf/PackageStates.php
@@ -0,0 +1,73 @@
+<?php
+# PackageStates.php
+
+# This file is maintained by TYPO3's package management. Although you can edit it
+# manually, you should rather use the extension manager for maintaining packages.
+# This file will be regenerated automatically if it doesn't exist. Deleting this file
+# should, however, never become necessary if you use the package commands.
+
+return [
+    'packages' => [
+        'core' => [
+            'packagePath' => 'typo3/sysext/core/',
+        ],
+        'scheduler' => [
+            'packagePath' => 'typo3/sysext/scheduler/',
+        ],
+        'extbase' => [
+            'packagePath' => 'typo3/sysext/extbase/',
+        ],
+        'fluid' => [
+            'packagePath' => 'typo3/sysext/fluid/',
+        ],
+        'install' => [
+            'packagePath' => 'typo3/sysext/install/',
+        ],
+        'indexed_search' => [
+            'packagePath' => 'typo3/sysext/indexed_search/',
+        ],
+        'extensionmanager' => [
+            'packagePath' => 'typo3/sysext/extensionmanager/',
+        ],
+        'lang' => [
+            'packagePath' => 'typo3/sysext/lang/',
+        ],
+        'backend' => [
+            'packagePath' => 'typo3/sysext/backend/',
+        ],
+        'cshmanual' => [
+            'packagePath' => 'typo3/sysext/cshmanual/',
+        ],
+        'filelist' => [
+            'packagePath' => 'typo3/sysext/filelist/',
+        ],
+        'frontend' => [
+            'packagePath' => 'typo3/sysext/frontend/',
+        ],
+        'impexp' => [
+            'packagePath' => 'typo3/sysext/impexp/',
+        ],
+        'recordlist' => [
+            'packagePath' => 'typo3/sysext/recordlist/',
+        ],
+        'rte_ckeditor' => [
+            'packagePath' => 'typo3/sysext/rte_ckeditor/',
+        ],
+        'saltedpasswords' => [
+            'packagePath' => 'typo3/sysext/saltedpasswords/',
+        ],
+        'sv' => [
+            'packagePath' => 'typo3/sysext/sv/',
+        ],
+        'bootstrap_package' => [
+            'packagePath' => 'typo3conf/ext/bootstrap_package/',
+        ],
+        'introduction' => [
+            'packagePath' => 'typo3conf/ext/introduction/',
+        ],
+        'crawler' => [
+            'packagePath' => 'typo3conf/ext/crawler/',
+        ],
+    ],
+    'version' => 5,
+];
